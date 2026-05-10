5ded2e8619d386be232e24c478ee4e5d0355acd7
mattyoungaviation-collab <mattyoungaviation@gmail.com>
2026-05-10 14:14:56 -0700

    Add matrix navigation link

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -9,6 +9,7 @@ export default function Layout({ children }: { children: any }) {
         <div className="space-x-4">
           <Link to="/home">My Home</Link>
           <Link to="/profitability">Profitability</Link>
+          <Link to="/matrix">Matrix</Link>
           <button
             onClick={() => {
               logout();
